5b22e4494b8742580492819ebdf6d2f97b1f256c
Zain Adam <zainadam.id@gmail.com>
2022-08-19 01:00:30 +0700

    feat: add tooltip component

---
+++ src/MaterialBlade/views/components/tooltip.blade.php
@@ -0,0 +1,40 @@
+@isset($body)
+    <span class="mdc-tooltip-wrapper--rich">
+@endisset
+    {!! $childPreprocess($slot->__tostring()) !!}
+
+    <div {{ $attributesPreprocess($attributes)->class([
+        'mdc-tooltip--rich' => isset($body),
+    ]) }}>
+        <div class="mdc-tooltip__surface mdc-tooltip__surface-animation">
+            @if(is_a($title, 'Illuminate\View\ComponentSlot'))
+
+                <h2 {{ $title->attributes->class(['mdc-tooltip__title']) }}>{{ $title }}</h2>
+            @endif
+
+            @isset($body)
+                <p {{ $body->attributes->class(['mdc-tooltip__content']) }}>
+                    {{ $body }}
+                </p>
+            @else
+                {{ $title }}
+            @endisset
+
+            @isset($action)
+                <span {{ $action->attributes->class(['mdc-tooltip--rich-actions']) }}>
+                    {{ $action }}
+                </span>
+            @endisset
+        </div>
+    </div>
+@isset($body)
+    </span>
+@endisset
+
+@once
+    @push('MaterialBlade-scripts-on-ready')
+        [...document.querySelectorAll('.mdc-tooltip')].map(tooltipEl => {
+        mdc.tooltip.MDCTooltip.attachTo(tooltipEl);
+        });
+    @endpush
+@endonce
