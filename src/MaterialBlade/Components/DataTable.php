ed825ab3ba301f05202d37af2e8d6c84bd5638e7
Zain Adam <zainadam.id@gmail.com>
2022-09-19 02:18:21 +0700

    feat: add DataTable Component

---
+++ src/MaterialBlade/Components/DataTable.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace MaterialBlade\Components;
+
+
+use Illuminate\View\Component;
+use Illuminate\View\ComponentAttributeBag;
+
+class DataTable extends Component
+{
+  public bool $isWithCheckbox;
+  public array $header;
+  public array $tableData;
+  /**
+   * Create a new component instance.
+   *
+   * @return void
+   */
+  public function __construct(
+    bool $withCheckbox = false,
+    array $header = [],
+    array $data = []
+  ) {
+    $this->isWithCheckbox = $withCheckbox;
+    $this->tableData = $data;
+    $this->header = $header;
+  }
+
+  /**
+   * Get the view / contents that represent the component.
+   *
+   * @return \Illuminate\Contracts\View\View|\Closure|string
+   */
+  public function render()
+  {
+    return view('MaterialBlade::data-table');
+  }
+}
