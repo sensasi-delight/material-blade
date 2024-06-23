c0d7152915379a942bbcacbbb37ed1d0f763089e
Zain Adam <zainadam.id@gmail.com>
2024-06-23 22:04:53 +0800

    Add the `List` component Fixes #18

---
+++ src/Components/MbcList/Enum/Variant.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace MaterialBlade\Components\MbcList\Enum;
+
+enum Variant: string
+{
+    case SINGLE_LINE = 'single-line';
+    case TWO_LINE = 'two-line';
+    case PADDED = 'padded';
+}
---
+++ src/views/components/list/item.blade.php
@@ -0,0 +1,26 @@
+@aware(['disableRipple' => false])
+
+<{{ $htmlElement }} {{ $attributesPreprocess($attributes) }}>
+
+    @if(!$disableRipple)
+    <span class="mdc-deprecated-list-item__ripple"></span>
+    @endif
+
+    @if($icon)
+    <x-mbc::icon aria-hidden="true" :icon="$icon" class="mdc-deprecated-list-item__graphic" />
+    @endif
+
+    <span class="mdc-deprecated-list-item__text">
+        @if($primaryText)
+        <span class="mdc-deprecated-list-item__primary-text">{{ $primaryText }}</span>
+        @endif
+
+        @if($secondaryText)
+        <span class="mdc-deprecated-list-item__secondary-text">{{ $secondaryText }}</span>
+        @endif
+
+        @isset($slot)
+        {{ $slot }}
+        @endisset
+    </span>
+</{{ $htmlElement }}>
