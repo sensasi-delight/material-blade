d1effeade1ed244297d14e034e3808186f396864
Zain Adam <zainadam.id@gmail.com>
2024-06-25 02:39:16 +0800

    add the `Drawer` Component Fixes #17

---
+++ src/views/components/drawer.blade.php
@@ -0,0 +1,17 @@
+<aside {{ $attributesPreprocess($attributes) }}>
+    @if($title || $subtitle)
+    <div class="mdc-drawer__header">
+        @if($title)
+        <div class="mdc-drawer__title">{{ $title }}</div>
+        @endif
+
+        @if($subtitle)
+        <div class="mdc-drawer__subtitle">{{ $subtitle }}</div>
+        @endif
+    </div>
+    @endif
+
+    <div class="mdc-drawer__content">
+        {{ $slot }}
+    </div>
+</aside>
