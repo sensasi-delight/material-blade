3fad1229d2e65de61d892c311758816be419f902
Zain Adam <zainadam.id@gmail.com>
2024-07-01 06:51:19 +0800

    fix: vercel can't detect render return string

--- src/Components/Grid.php
+++ src/Components/Grid.php
@@ -2,6 +2,7 @@
 
 namespace MaterialBlade\Components;
 
+use Illuminate\Support\Facades\View;
 use Illuminate\View\Component;
 use Illuminate\View\ComponentAttributeBag;
 
@@ -78,10 +79,10 @@ class Grid extends Component
     public function render()
     {
         if ($this->isContainer) {
-            return 'mbv::grid.container';
+            return View::make('mbv::grid.container');
         }
 
-        return 'mbv::grid.item';
+        return View::make('mbv::grid.item');
     }
 
     public function attributesPreprocess(ComponentAttributeBag $attributes)
